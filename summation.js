e548a9be3344b5981b3c8dc981bfc5e9586965b9
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-03-13 23:35:22 +0600

    summation problem added

---
+++ summation.js
@@ -0,0 +1,18 @@
+/** 1 to n summation
+ * 
+ */
+
+// let num1 = 1;
+// let num2 = 2;
+
+// let sum =0;
+// sum = num1 + num2;
+// console.log(sum);
+
+let n = 10;
+let sum = 0;
+for(i = 1; i <= n; i++){
+    sum = sum + i;
+}
+
+console.log(sum);
