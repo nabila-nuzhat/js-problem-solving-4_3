1c7e09a50c1c4f31af8b66e2dddad44cb4dac297
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-03-14 06:53:22 +0600

    problem 2 timestable with js

---
+++ timesTable.js
@@ -0,0 +1,15 @@
+
+/** write times Table of 3 with JS
+ * 1 * 3  = 3
+ * 2 * 3  = 6
+ * 3 * 3  = 9
+ * 4 * 3  = 12
+ * ..........
+ * 10 * 3  = 30
+ * 
+ * 1 to 10 numbers multiply with 3 every time
+ */
+
+for(let i = 1; i<= 10; i++){
+    console.log(i +" * 3 = " + i*3);
+}
