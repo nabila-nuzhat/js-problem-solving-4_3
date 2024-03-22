9023427489ed9e738e4c87cb10511de9590884b6
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-03-22 22:39:22 +0600

    longest name problem add

--- longestName.js
+++ longestName.js
@@ -4,7 +4,8 @@
  * output: radnaxelA
  */
 
-function reverseLongestName(name1, name2){
+/**
+ * function reverseLongestName(name1, name2){
     let nameOneLength = name1.length;
     let nameTwoLength = name2.length;
 
@@ -27,3 +28,46 @@ console.log(name2.split("").reverse());
 let resultRev = reverseLongestName("John", "Alexandar");
 
 console.log(resultRev);
+ */
+
+
+
+
+// OPTIONAL OPTION REVERSE-----------
+
+// let str = "Alexis";
+
+// for(let i=0; i<str.length; i++){
+//     const element = str[i];
+//     console.log(element);
+// }
+
+let str = "Alexis";
+let reversedString = "";
+for(let i = str.length-1; i>=0; i--){
+    const element =str[i];
+    reversedString = reversedString + element;
+}
+
+console.log(reversedString);
+
+// LONGEST NAME REVERSE function option 2
+function reverseLongestName(name1, name2){
+    let nameOneLength = name1.length;
+    let nameTwoLength = name2.length;
+
+    let largeNmae = "";
+
+    if(nameOneLength > nameTwoLength){
+        largeNmae = name1;
+    }
+
+    else{
+        largeNmae = name2;
+    }
+    return largeNmae
+}
+
+let result = reverseLongestName("John", "Alexandar");
+
+console.log(result);
